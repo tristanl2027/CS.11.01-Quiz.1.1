b168d54fd81e8146a12c77c6a018cc37ff96893b
tristanl2027 <tristanl2027@student.cis.edu.hk>
2024-08-28 09:25:44 +0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,26 @@ import java.util.Scanner;
 *****************************************************************************************************
 * By writing your name in the space provided below, you are honouring the CIS academic honesty pledge 
 * “I have neither given nor received unauthorized aid on this piece of work.”
-* NAME: 
+* NAME: Tristan Lui
 *****************************************************************************************************
 */
 
 public class Main {
 
     public static void main(String[] args) {
-
-
-
-
+        Scanner scanner = new Scanner(System.in);
+        int age;
+        String firstName;
+        String favouriteFood;
+        System.out.println("Enter your age: ");
+        age = scanner.nextInt();
+        System.out.println("Enter your first name: ");
+        firstName = scanner.next();
+        System.out.println("Enter your favourite food: ");
+        favouriteFood = scanner.next();
+        System.out.println("First name: "+firstName);
+        System.out.println("Age: "+age);
+        System.out.println("Favourite food: "+favouriteFood);
     }
 
 }
